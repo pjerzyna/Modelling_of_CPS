80a0555826d1a396bb28bc28b0eda1e7c1aa7f98
pjerzyna <pawel.jerzyna@interia.pl>
2026-06-02 14:22:24 +0200

    arm and pendulum

---
+++ main.m
@@ -0,0 +1,10 @@
+m = 0.2;
+l = 0.5;
+g = 9.81;
+b = 0.4;
+
+% Link do notatek google:
+% https://docs.google.com/document/d/18R_7BUF4b25SAKLMLjVrlbADpHYMjgFTGn7Dtx8Om8U/edit?usp=sharing
+
+% W pliku - inverted_pendulum_on_a_cart_controlled - zostal zamodelowany 
+% wozek z masa, ktore utrzymuja rownowage (na podstawie trzeciego filmiku)
