9ef8db127d0dba9cff65791401a02435acc8fdfa
pjerzyna <pawel.jerzyna@interia.pl>
2026-06-11 01:04:48 +0200

    concept of swing up mechanism

--- main.m
+++ main.m
@@ -1,16 +1,7 @@
-kp_pend = 1;
-kd_pend = 0.1;
-ki_pend = 0.01;
-
-
-
-kp_arm = 0.3;
-kd_arm = 5;
-ki_arm = 1;
+g = 9.81;
+m_pendulum = 0.405;
+l_pendulum = 0.6;
 
 
 % Link do notatek google:
 % https://docs.google.com/document/d/18R_7BUF4b25SAKLMLjVrlbADpHYMjgFTGn7Dtx8Om8U/edit?usp=sharing
-
-% W pliku - inverted_pendulum_on_a_cart_controlled - zostal zamodelowany 
-% wozek z masa, ktore utrzymuja rownowage (na podstawie trzeciego filmiku)
