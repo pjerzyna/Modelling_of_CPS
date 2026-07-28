c54fc1eceff7c28225cc5139949fb8151000ef68
pjerzyna <pawel.jerzyna@interia.pl>
2026-07-29 00:26:58 +0200

    readme done

--- main.m
+++ main.m
@@ -3,5 +3,10 @@ m_pendulum = 0.405;
 l_pendulum = 0.6;
 
 
-% Main Furuta model and previously created easier reference models:
-% https://github.com/pjerzyna/Modelling_of_CPS
+% Data for main furuta model and previously created easier reference models:
+% pendulum: https://www.youtube.com/watch?v=LCuPclioMc4&t=1282s
+% pendulum on a cart: https://www.youtube.com/watch?v=TJeTHyAWU7Y&t=13s
+% pendulum on a cart PID control: https://www.youtube.com/watch?v=6MZclvbTTIU
+
+
+
